6b044ead55a44a7df655de6efa7435cda784c9b9
Akshay S Dinesh <asdofindia@gmail.com>
2020-01-24 20:17:12 +0530

    feat: alternate mongodb schema

--- src/main/java/org/metastringfoundation/healthheatmap/MongoDB.java
+++ src/main/java/org/metastringfoundation/healthheatmap/MongoDB.java
@@ -2,18 +2,18 @@ package org.metastringfoundation.healthheatmap;
 
 
 import com.mongodb.MongoClient;
-import static com.mongodb.client.model.Filters.*;
-import static com.mongodb.client.model.Updates.*;
-
-import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.UpdateOptions;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.*;
 
 public class MongoDB implements Database {
     private MongoCollection<Document> collectionOfDatasets;
@@ -41,9 +41,8 @@ public class MongoDB implements Database {
 
         String datasetName = metadata.getName();
 
-        UpdateOptions upsert = new UpdateOptions().upsert(true);
 
-        Bson datasetFilter = eq("name", datasetName);
+        Bson datasetFilter = eq("_metadata.name", datasetName);
         List<Bson> datasetUpdates = new ArrayList<>();
 
         for (int entityIndex = 0; entityIndex < entities.size(); entityIndex++) {
@@ -58,6 +57,8 @@ public class MongoDB implements Database {
         }
 
         Bson datasetUpdate = combine(datasetUpdates);
+
+        UpdateOptions upsert = new UpdateOptions().upsert(true);
         this.collectionOfDatasets.updateOne(
                 datasetFilter,
                 datasetUpdate,
@@ -66,6 +67,36 @@ public class MongoDB implements Database {
     }
 
     private void addDatasetAsEntities(Dataset dataset) {
-        
+        List<String> indicators = dataset.getIndicators();
+        List<String> entities = dataset.getEntities();
+        List<List<DataPoint>> dataGroupedByEntities = dataset.getDataGroupedByEntities();
+        DatasetMetadata metadata = dataset.getMetadata();
+
+        String datasetName = metadata.getName();
+
+        for (int entityIndex = 0; entityIndex < entities.size(); entityIndex++) {
+            String entityName = entities.get(entityIndex);
+            Bson datasetFilter = eq("name", entityName);
+
+            List<Bson> datasetUpdates = new ArrayList<>();
+
+            for (int indicatorIndex = 0; indicatorIndex < indicators.size(); indicatorIndex++) {
+                String indicatorName = indicators.get(indicatorIndex);
+                String fieldToUpdate = indicatorName;
+                Object value = dataGroupedByEntities.get(entityIndex).get(indicatorIndex).getValue();
+                Document dataDoc = new Document();
+                dataDoc.append("value", value);
+                dataDoc.append("source", datasetName);
+                datasetUpdates.add(addToSet(fieldToUpdate, dataDoc));
+            }
+            Bson datasetUpdate = combine(datasetUpdates);
+
+            UpdateOptions upsert = new UpdateOptions().upsert(true);
+            this.collectionOfEntities.updateOne(
+                    datasetFilter,
+                    datasetUpdate,
+                    upsert
+            );
+        }
     }
 }
