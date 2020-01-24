d4b04d76a000c40e41ad9b0ea47b94401567b493
Akshay S Dinesh <asdofindia@gmail.com>
2020-01-24 20:17:45 +0530

    feat: better CLI

--- src/main/java/org/metastringfoundation/healthheatmap/CLI.java
+++ src/main/java/org/metastringfoundation/healthheatmap/CLI.java
@@ -1,13 +1,37 @@
 package org.metastringfoundation.healthheatmap;
 
+import org.apache.commons.cli.*;
+
 public class CLI {
-    public String parse(String[] args) throws IllegalArgumentException {
+    //TODO convert the other builder in this project to static class
+    final static Option datasetName = Option.builder("n")
+            .hasArg()
+            .longOpt("DATASET_NAME_OPTION")
+            .build();
+    final static Option path = Option.builder("p")
+            .required()
+            .hasArg()
+            .longOpt("PATH_OPTION")
+            .desc("Path to the file")
+            .build();
+
+    public final static Options options = new Options()
+            .addOption(path)
+            .addOption(datasetName);
+
+    public CommandLine parse(String[] args) throws IllegalArgumentException, ParseException {
+
+        final CommandLineParser commandLineParser = new DefaultParser();
         try {
-            String absolutePath = new PathManager(args[0]).getAbsolutePath();
-            return absolutePath;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("You need to call this program with path to csv file as first argument");
-            throw new IllegalArgumentException(e);
+            return commandLineParser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e);
+            throw e;
         }
+    }
+
+    public static void printHelp() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("data-reader", options);
     }
 }
--- src/main/java/org/metastringfoundation/healthheatmap/Main.java
+++ src/main/java/org/metastringfoundation/healthheatmap/Main.java
@@ -1,17 +1,34 @@
 package org.metastringfoundation.healthheatmap;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+
 public class Main {
 
-    public static void main(String[] args) throws DatasetIntegrityError, IllegalArgumentException {
-        String path = new CLI().parse(args);
-        Dataset dataset = new CSVDataset
-                .builder()
-                .path(path)
-                .build();
+    public static void main(String[] args) throws IllegalArgumentException {
 
-        DataStorer dataStorer = new MultiplexDataStorer();
+        try {
+            CommandLine commandLine = new CLI().parse(args);
 
-        dataStorer.addDataset(dataset);
+            String path = commandLine.getOptionValue("PATH_OPTION");
+            String datasetName = commandLine.getOptionValue("DATASET_NAME_OPTION");
 
+            Dataset dataset = new CSVDataset
+                    .builder()
+                    .metadata(new DatasetMetadata(datasetName))
+                    .path(path)
+                    .build();
+
+            DataStorer dataStorer = new MultiplexDataStorer();
+
+            dataStorer.addDataset(dataset);
+
+        } catch (ParseException e) {
+            CLI.printHelp();
+            System.exit(1);
+        } catch (DatasetIntegrityError e) {
+            System.out.println("The data has some problem");
+            e.printStackTrace();
+        }
     }
 }
