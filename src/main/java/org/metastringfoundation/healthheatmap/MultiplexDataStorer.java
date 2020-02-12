622ddc8de2eb7074c3832420eef3f60a32d1295a
Akshay S Dinesh <asdofindia@gmail.com>
2020-02-12 10:25:40 +0530

    refactor: remove unused MongoDB

--- src/main/java/org/metastringfoundation/healthheatmap/MultiplexDataStorer.java
+++ src/main/java/org/metastringfoundation/healthheatmap/MultiplexDataStorer.java
@@ -17,17 +17,14 @@
 package org.metastringfoundation.healthheatmap;
 
 public class MultiplexDataStorer implements DataStorer {
-    private Database mongodb;
     private Database postgres;
 
     public MultiplexDataStorer() {
-        this.mongodb = new MongoDB();
         this.postgres = new PostgreSQL();
     }
 
     @Override
     public void addDataset(Dataset dataset) {
-        this.mongodb.addDataset(dataset);
         this.postgres.addDataset(dataset);
     }
 }
