23988819bf36166dc14953d18d89014771b6aa03
Akshay S Dinesh <asdofindia@gmail.com>
2020-02-29 23:43:07 +0530

    feat: equals, hashcode

--- src/main/java/org/metastringfoundation/healthheatmap/dataset/CSVCell.java
+++ src/main/java/org/metastringfoundation/healthheatmap/dataset/CSVCell.java
@@ -16,6 +16,8 @@
 
 package org.metastringfoundation.healthheatmap.dataset;
 
+import java.util.Objects;
+
 public class CSVCell {
     private int row;
     private int column;
@@ -55,5 +57,30 @@ public class CSVCell {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (super.equals(obj)) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        CSVCell other = (CSVCell) obj;
+        return (other.getColumn() == this.getColumn() &&
+                other.getRow() == this.getRow() &&
+                other.getValue().equals(this.getValue())
+        );
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n" +
+                "column: " + getColumn() + "\n" +
+                "row: " + getRow() + "\n"  +
+                "value: " + getValue();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column, value);
     }
 }
